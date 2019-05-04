057d9ad5f2dbd21b94ebc80fedec015d8bf3ef53
FirmanJabar <firmanabduljabar@gmail.com>
2019-05-04 12:22:43 +0800

    first deploy

---
+++ index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from premiumlayers.net/demo/html/monsterrat/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 02 Feb 2019 03:01:05 GMT -->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>IVVIIXVII - Firman Jabar</title>
+<meta name="keywords" content="HTML5,CSS3,HTML,Template,Multi-Purpose,firmanJabar,Corporate Theme,HTML5 Portfolio Template">
+<meta name="description" content="Monsterrat - Minimal HTML5 Portfolio">
+<meta name="author" content="firmanJabar">
+
+<!-- FONTS ONLINE -->
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+<!-- CSS TAMBAHAN -->
+<link rel="shortcut icon" href="assets/images/favicon.ico">
+<link rel="manifest" href="./manifest.json" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<!--MAIN STYLE-->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/main.css" rel="stylesheet" type="text/css">
+<link href="assets/css/style.css" rel="stylesheet" type="text/css">
+<link href="assets/css/responsive.css" rel="stylesheet" type="text/css">
+<link href="assets/css/animate.css" rel="stylesheet" type="text/css">
+<link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/ionicons.min.css" rel="stylesheet" type="text/css">
+<link href="assets/css/drop-down-menu.css" rel="stylesheet" type="text/css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+
+<!--======= WRAP =========-->
+<div id="wrap">
+  <div class="portfolio-wrapper"> 
+    <!--======= HEADER =========-->
+    <header>
+      <nav>
+      
+      <!--======= Close nav =========-->
+      <div class="nav-close">
+          <a href="#"><i class="ion-ios-close-outline"></i></a>
+      </div>
+        
+        
+      <div class="navgation"> 
+
+      <a href="#"><i class="ion-navicon fa-2x"></i></a>
+        <div class="open-navigation">
+          <div class="navi-in animated flipInY"> 
+            <!--======= LOGO =========--> 
+            <a href="index.php" class="logo"> <img src="assets/images/logo.png" alt="" > <span>- IVVIIXVII -<br>Firman Jabar</span>
+            <hr>
+            </a> 
+            <!--======= NAV LINKS =========-->
+            <ul class="nav-links">
+              <li>
+                <a href="index.php">PORTFOLIO</a>
+              </li>
+              <li>
+                <a href="about.php">ABOUT</a>
+              </li>
+              <li>
+                <a href="#"><del>BLOG</del></a>
+              </li>
+              <li>
+                <a href="contact.php">CONTACT</a>
+              </li>
+            </ul>
+            <!--======= Address =========-->
+            <p class="ads">246 Banjarbaru City<br>South Kalimantan, IDN</p>
+          </div>
+        </div>
+      </div>
+
+      <!--======= NAV FILTERS =========-->
+      <div class="navgation"> <a href="#"><i class="ion-ios-keypad-outline"></i></a>
+        <div class="open-navigation">
+          <div class="navi-in animated flipInY"> 
+            <!--======= LOGO =========--> 
+            <a href="#" class="logo"> <img src="assets/images/portfolio-grid.png" alt="" > <span>PORTFOLIO</span>
+            <hr>
+            </a> 
+            <!--======= NAV LINKS =========-->
+            <ul class="nav-links filter">
+              <li><a class="active" href="#." data-filter="*">ALL</a></li>
+              <li><a href="#" data-filter=".design">Design</a></li>
+              <li><a href="#" data-filter=".random">Random</a></li>
+              <li><a href="#" data-filter=".photo">Photography</a></li>
+              <li><a href="#" data-filter=".event">Event</a></li>
+            </ul>
+            <!--======= Address =========-->
+            <p class="ads">246 Banjarbaru City<br>South Kalimantan, IDN</p>
+          </div>
+        </div>
+      </div>
+
+      <!--======= SOCIAL ICONS =========-->
+      <div class="navgation share"> <a href="#"><i class="ion-android-share-alt"></i></a>
+        <div class="open-navigation">
+          <div class="navi-in animated flipInY">
+            <ul class="social_icons">
+              <li class="facebook"><a href="https://facebook.com/firmanabduljabar"><i class="fa fa-facebook"></i> </a></li>
+              <li class="twitter"><a href="https://twitter.com/firmanjabar"><i class="fa fa-twitter"></i> </a></li>
+              <li class="flickr"><a href="https://www.instagram.com/firmanjabar/"><i class="fa fa-instagram"></i> </a></li>
+              <li class="instagram"><a href="https://www.instagram.com/firmanjabar/"><i class="fa fa-instagram"></i> </a></li>
+              <li class="googleplus"><a href="https://m.youtube.com/channel/UCA2OGjmsBZ1wfARf0cTUHiA"><i class="fa fa-youtube"></i> </a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <!--======= SEARCH ICON =========--> 
+      <a href="#" class="open-search header-open-search"><i class="ion-ios-search"></i></a> 
+
+
+      </nav>
+    </header>
+    
+    <!--======= HOME PORTFOLIO =========-->
+<div class="portfolio">
+  <div class="container-fluid">
+    <ul class="row items">
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding "> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/0.png" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Firman</span>
+          <hr>
+          <span class="cat">Logo</span> </a> </div>
+      </li>
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding photo"> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/5.jpg" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Matasirih</span>
+          <hr>
+          <span class="cat">Photography</span> </a> </div>
+      </li>
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding"> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/1.png" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Firman</span>
+          <hr>
+          <span class="cat">Logo</span> </a> </div>
+      </li>
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding design"> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/6.png" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Firman for President</span>
+          <hr>
+          <span class="cat">Design</span> </a> </div>
+      </li>
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding "> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/2.png" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Firman</span>
+          <hr>
+          <span class="cat">Logo</span> </a> </div>
+      </li>
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding photo"> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/7.jpg" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Ekspedisi Nusantara Jaya</span>
+          <hr>
+          <span class="cat">Photography</span> </a> </div>
+      </li>
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding"> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/4.png" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Firman</span>
+          <hr>
+          <span class="cat">Logo</span> </a> </div>
+      </li>
+      
+      <!-- Portfolio Item -->
+      <li class="item col-md-3 no-padding design"> 
+        <!-- Image --> 
+        <img class="img-responsive" src="assets/images/8.png" alt="" > 
+        <!-- Hover Section -->
+        <div class="hover-sec"> 
+          <!-- Hover Link --> 
+          <a href="#." class="hover-in"> <span class="name">Firman Potter's</span>
+          <hr>
+          <span class="cat">Design</span> </a> </div>
+      </li>
+    </ul>
+
+    <div class="rights">
+      <p>IVVIIXVII - Firman Jabar<br>Copyright 2019.  All Rights Reserved</p>
+    </div>
+  </div>
+</div>
+
+  </div>
+</div>
+<!-- Wrap End --> 
+<!-- Search -->
+<div class="search-overlay"></div>
+<div class="search"> <a href="#" class="search-close"><i class="ion-android-close"></i></a>
+  <div class="row">
+    <div class="col-sm-6 col-sm-offset-3">
+      <form class="search-form">
+        <input type="text" id="search" name="search" class="text-center" placeholder="Search" />
+        <button type="submit"><i class="ion-ios-search-strong"></i></button>
+      </form>
+      <!-- end .search-form --> 
+    </div>
+    <!-- end .col-sm-6 --> 
+  </div>
+  <!-- end .row --> 
+</div>
+<script src="assets/js/jquery-1.11.0.min.js"></script> 
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/waypoints.min.js"></script> 
+<script src="assets/js/counter.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script> 
+<script src="assets/js/jquery.isotope.min.js"></script> 
+<script src="assets/js/search-pop-up.js"></script> 
+<script src="assets/js/main.js"></script> 
+<script src="https://apis.google.com/js/platform.js"></script>
+
+<script>
+  // REGISTER SERVICE WORKER
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(function () {
+          console.log("Pendaftaran ServiceWorker berhasil");
+        })
+        .catch(function () {
+          console.log("Pendaftaran ServiceWorker gagal");
+        });
+    });
+  } else {
+    console.log("ServiceWorker belum didukung browser ini.");
+  }
+
+  // Periksa fitur Notification API
+  if ("Notification" in window) {
+    requestPermission();
+  } else {
+    console.error("Browser tidak mendukung notifikasi.");
+  }
+
+  // Meminta ijin menggunakan Notification API
+  function requestPermission() {
+    if ('Notification' in window) {
+      Notification.requestPermission().then(function (result) {
+        if (result === "denied") {
+          console.log("Fitur notifikasi tidak diijinkan.");
+          return;
+        } else if (result === "default") {
+          console.error("Pengguna menutup kotak dialog permintaan ijin.");
+          return;
+        }
+
+      });
+    }
+  }
+</script>
+</body>
+
+<!-- Mirrored from premiumlayers.net/demo/html/monsterrat/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 02 Feb 2019 03:02:05 GMT -->
+</html>
