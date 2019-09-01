e8b6da3cb40917bcfae225425fc0e5957899a9de
FirmanJabar <firmanabduljabar@gmail.com>
2019-09-01 20:28:45 +0800

    change project

--- js/app.js
+++
@@ -1,6 +0,0 @@
-// REGISTER SERVICE WORKER
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(reg => console.log('service worker registered'))
-        .catch(err => console.log('service worker not registered', err));
-}
